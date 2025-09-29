f67168d9a8841d84365e614e4494bbc9baa7b90d
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-09-29 10:41:59 +0100

    Added HasUserRatedMovieAsync and other query methods to IReviewRepository and added rating-related methods to IMovieRepository

--- InzRate.Core.Application/Contracts/Persistence/IMovieRepository.cs
+++ InzRate.Core.Application/Contracts/Persistence/IMovieRepository.cs
@@ -7,6 +7,9 @@ public interface IMovieRepository
     Task<Movie?> GetByIdAsync(Guid id);
     Task<IEnumerable<Movie>> GetAllAsync();
     Task<Movie?> GetByTitleAsync(string title);
+    Task<IEnumerable<Movie>> GetByReleaseYearAsync(int releaseYear);
+    Task<IEnumerable<Movie>> GetWithAverageRatingAsync();
+    Task<double?> GetAverageRatingAsync(Guid movieId);
     Task AddAsync(Movie movie);
     void Update(Movie movie);
     void Delete(Movie movie);
--- InzRate.Core.Application/Contracts/Persistence/IReviewRepository.cs
+++ InzRate.Core.Application/Contracts/Persistence/IReviewRepository.cs
@@ -6,6 +6,10 @@ public interface IReviewRepository
 {
     Task<Review?> GetByIdAsync(Guid id);
     Task<IEnumerable<Review>> GetAllAsync();
+    Task<IEnumerable<Review>> GetByMovieIdAsync(Guid movieId);
+    Task<IEnumerable<Review>> GetByUserIdAsync(Guid userId);
+    Task<Review?> GetByUserAndMovieIdAsync(Guid userId, Guid movieId);
+    Task<bool> HasUserRatedMovieAsync(Guid userId, Guid movieId);
     Task AddAsync(Review review);
     void Update(Review review);
     void Delete(Review review);
