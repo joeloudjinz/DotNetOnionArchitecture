fc4dc9172e92a1bd95353a8dd624c6497b55fa81
Abdullah Addoun <addoun.abdellah@gmail.com>
2025-09-29 10:52:58 +0100

    Added CreateReviewCommandValidator with FluentValidation rules for UserId, MovieId, Stars (1-5 range), and Text validation

---
+++ InzRate.Core.Application/Features/Reviews/Commands/CreateReview/CreateReviewCommandValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using InzRate.Core.Application.Features.Reviews.Commands.CreateReview;
+
+namespace InzRate.Core.Application.Features.Reviews.Commands.CreateReview;
+
+public class CreateReviewCommandValidator : AbstractValidator<CreateReviewCommand>
+{
+    public CreateReviewCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.MovieId)
+            .NotEmpty().WithMessage("Movie ID is required.");
+
+        RuleFor(x => x.Stars)
+            .InclusiveBetween(1, 5).WithMessage("Stars must be between 1 and 5.");
+
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Review text is required.")
+            .MaximumLength(1000).WithMessage("Review text cannot exceed 1000 characters.");
+    }
+}
